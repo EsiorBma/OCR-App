4943e7b5274f223eca1e18ed00dcab9989723f90
Ambroise KOUWADAN <142091904+EsiorBma@users.noreply.github.com>
2025-07-11 03:36:06 +0000

    Update2 build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Mettre à jour le système
-sudo apt-get update
+apt-get update
 
 # Installer Tesseract OCR et la langue française
-sudo apt-get install -y tesseract-ocr
-sudo apt-get install -y tesseract-ocr-fra
+apt-get install -y tesseract-ocr
+apt-get install -y tesseract-ocr-fra
 
 # Installer les dépendances système pour PyMuPDF
-sudo apt-get install -y libgl1 libglib2.0-0
+apt-get install -y libgl1 libglib2.0-0
 
 # Créer le dossier temporaire pour les uploads
 mkdir -p /tmp/uploads
